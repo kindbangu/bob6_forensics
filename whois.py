c80fad0bbfcb29b4c386e9957adf2b444725b740
root <root@localhost.localdomain>
2017-07-16 08:46:32 -0400

    add whois

---
+++ whois.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import pythonwhois
+import json
+import sys
+
+f_domain = open('domain.txt','r') #input domain
+domain_data = f_domain.read().split()
+
+def date_handler(obj):      #this funciton in order to catch timeerror
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError
+
+f_result = open(sys.argv[1],'w')
+
+for line in domain_data:
+    data = pythonwhois.get_whois(line)
+    json_data = json.dumps(data,default=date_handler)
+    f_result.write(str(json_data))
+print "Complete!!\nCheck result.txt!"
+f_result.close()
+f_domain.close()
